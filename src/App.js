21b83076072238ee6245859ce119fa9f4104dcc7
kiurd-gentelman <k.r.imtiaz@gmail.com>
2021-06-20 18:10:32 +0600

    project push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import NavBar from './Components/NavBarComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+
+    </div>
+  );
+}
+
+export default App;
