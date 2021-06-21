ed9514a9548223e8b2ce05c283ae0bad86ae74b4
kiurd_gentleman <k.r.imtiaz@gmail.com>
2021-06-22 02:24:50 +0600

    project all post api settings

--- src/Pages/AllPost.js
+++ src/Pages/AllPost.js
@@ -1,57 +1,140 @@
 import React , { useState } from 'react'
 import ReactPaginate from 'react-paginate'
 import JsonData from './../data/user_list.json'
+import axios from 'axios';
 
 // import { ListGroup } from 'react-bootstrap'
 
-export const AllPost = () => {
-    // const [users,setUsers] = useState(JsonData.slice(0, 50));
+class AllPost extends React.Component {
     const [users] = useState(JsonData.slice(0, 300));
     const [pageNumber, setPageNumber] = useState(0);
 
     const usersPerPage = 10;
     const pagesVisited = pageNumber * usersPerPage;
     const pageCount = Math.ceil(users.length / usersPerPage);
-
     const displayUsers = users.slice(pagesVisited, pagesVisited + usersPerPage).map((user) => {
-      return (
+        return (
             <div >
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                     <div className="ms-2 me-auto">
                         <span className="fw-bold">Title section</span><span><sub>{"  "}{"  "}Author {user.firstName} .  posted in {user.lastName} . {user.email} </sub></span>
                         <div>This is some description for post , just two or 3 line post in to this post</div>
-                        
+
                     </div>
                     <span className="btn btn-sm  btn-primary rounded-pill">View Post</span>
                 </li>
             </div>
-      );
+        );
     });
-
-
     const changePage = ({ selected }) => {
         setPageNumber(selected);
-      };
-    return (
-        <div >
-            <ol className="list-group list-group-numbered">
-                {displayUsers} 
-            </ol>
-            
-            
-            <ReactPaginate
-                previousLabel={"Previous"}
-                nextLabel={"Next"}
-                pageCount={pageCount}
-                onPageChange={changePage}
-                containerClassName={"paginationBttns"}
-                previousLinkClassName={"previousBttn"}
-                nextLinkClassName={"nextBttn"}
-                disabledClassName={"paginationDisabled"}
-                activeClassName={"paginationActive"}
-            />
-        </div>
-    )
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+
+
+    }
+
+    componentDidMount() {
+        document.title = `You clicked ${this.state.count} times`;
+    }
+    componentDidUpdate() {
+        document.title = `You clicked ${this.state.count} times`;
+    }
+
+    render() {
+        return (
+            <div >
+                <ol className="list-group list-group-numbered">
+                    {this.displayUsers}
+                </ol>
+
+
+                <ReactPaginate
+                    previousLabel={"Previous"}
+                    nextLabel={"Next"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"paginationBttns"}
+                    previousLinkClassName={"previousBttn"}
+                    nextLinkClassName={"nextBttn"}
+                    disabledClassName={"paginationDisabled"}
+                    activeClassName={"paginationActive"}
+                />
+            </div>
+        );
+    }
 }
+
+
+
+
+
+//
+// export const AllPost = () => {
+//     // const [users,setUsers] = useState(JsonData.slice(0, 50));
+//
+//
+//      componentDidMount =() => {
+//         axios.get(`https://jsonplaceholder.typicode.com/users`)
+//           .then(res => {
+//             const persons = res.data;
+//             this.setState({ persons });
+//           })
+//       }
+//
+//
+//
+//     const [users] = useState(JsonData.slice(0, 300));
+//     const [pageNumber, setPageNumber] = useState(0);
+//
+//     const usersPerPage = 10;
+//     const pagesVisited = pageNumber * usersPerPage;
+//     const pageCount = Math.ceil(users.length / usersPerPage);
+//
+//     const displayUsers = users.slice(pagesVisited, pagesVisited + usersPerPage).map((user) => {
+//       return (
+//             <div >
+//                 <li className="list-group-item d-flex justify-content-between align-items-start">
+//                     <div className="ms-2 me-auto">
+//                         <span className="fw-bold">Title section</span><span><sub>{"  "}{"  "}Author {user.firstName} .  posted in {user.lastName} . {user.email} </sub></span>
+//                         <div>This is some description for post , just two or 3 line post in to this post</div>
+//
+//                     </div>
+//                     <span className="btn btn-sm  btn-primary rounded-pill">View Post</span>
+//                 </li>
+//             </div>
+//       );
+//     });
+//     const changePage = ({ selected }) => {
+//         setPageNumber(selected);
+//     };
+//
+//
+//
+//     return (
+//         <div >
+//             <ol className="list-group list-group-numbered">
+//                 {displayUsers}
+//             </ol>
+//
+//
+//             <ReactPaginate
+//                 previousLabel={"Previous"}
+//                 nextLabel={"Next"}
+//                 pageCount={pageCount}
+//                 onPageChange={changePage}
+//                 containerClassName={"paginationBttns"}
+//                 previousLinkClassName={"previousBttn"}
+//                 nextLinkClassName={"nextBttn"}
+//                 disabledClassName={"paginationDisabled"}
+//                 activeClassName={"paginationActive"}
+//             />
+//         </div>
+//     )
+// }
 
 export default AllPost;
