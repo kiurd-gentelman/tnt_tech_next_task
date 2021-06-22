e4da7ca0157fbcacb56172e08ba4a326a9fa688c
kiurd-gentelman <k.r.imtiaz@gmail.com>
2021-06-22 14:57:54 +0600

    post get and onclick button work

--- src/Pages/AllPost.js
+++ src/Pages/AllPost.js
@@ -1,92 +1,73 @@
-import React  , { useState ,useEffect  } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactPaginate from 'react-paginate'
-import JsonData from './../data/user_list.json'
+// import JsonData from './../data/user_list.json'
 import axios from 'axios';
 
 
 export const AllPost = () => {
-    // const [users,setUsers] = useState(JsonData.slice(0, 50));
-
-
-    let allUser ='';
+    let allUser = '';
     useEffect(() => {
         getAllNotes();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+    }, []);
 
-    const [users,setUsers] = useState('');
-    const  getAllNotes =() => {
-         axios.get(`http://127.0.0.1:8000/api/all-post`)
-            .then(res => {
-                const persons = res.data;
-                // console.log(persons)
-                // eslint-disable-next-line react-hooks/rules-of-hooks
-                allUser = persons.result;
-                console.log(allUser.slice(0, 1))
-                console.log(allUser.length)
-                // this.setState({ persons });
-                this.setState({ users: allUser })
-            })
-        // return allUser;
-    }
-    // console.log(getAllNotes)
+    const [post, setPost] = useState([]);
+    const getAllNotes = () => {
+            axios.get(`http://127.0.0.1:8000/api/all-post`)
+                .then(res => {
+                    const persons = res.data;
+                    allUser = persons.result;
+                    setPost(allUser )
+                })
 
-// console.log(JsonData.slice(0, 50));
-
-    // const [users] = data;
+        }
 
     const [pageNumber, setPageNumber] = useState(0);
+    const postPerPage = 10;
+    const pagesVisited = pageNumber * postPerPage;
+    const pageCount = Math.ceil(post.length / postPerPage);
+    const post_view = id =>{
 
-    const usersPerPage = 10;
-    const pagesVisited = pageNumber * usersPerPage;
-    const pageCount = Math.ceil(users.length / usersPerPage);
+        console.log(id)
+    }
 
-
-
-    console.log(users);
-    exit()
-
-
-
-    const displayUsers = users.slice(pagesVisited, pagesVisited + usersPerPage).map((user) => {
-      return (
-            <div >
-                <li className="list-group-item d-flex justify-content-between align-items-start" key={Math.random()}>
+    const displayUsers = post.slice(pagesVisited, pagesVisited + postPerPage).map((post) => {
+        return (
+            <div key={post.id}>
+                <li className="list-group-item d-flex justify-content-between align-items-start">
                     <div className="ms-2 me-auto">
-                        <span className="fw-bold">Title section</span><span><sub>{"  "}{"  "}Author {user.firstName} .  posted in {user.lastName} . {user.email} </sub></span>
-                        <div>This is some description for post , just two or 3 line post in to this post</div>
-
+                        <span className="fw-bold">{post.title}</span><span><sub>Author {post.author_id} .  posted in {post.created_at} </sub></span>
+                        <div>{post.description}</div>
                     </div>
-                    <span className="btn btn-sm  btn-primary rounded-pill">View Post</span>
+                    <span className="btn btn-sm  btn-primary rounded-pill" onClick={() => post_view(post.id)}>View Post</span>
                 </li>
             </div>
-      );
+        );
     });
+
+
     const changePage = ({ selected }) => {
         setPageNumber(selected);
     };
 
 
 
+
     return (
         <div >
-            <ol className="list-group list-group-numbered">
-                {displayUsers}
-            </ol>
+        <ol className = "list-group list-group-numbered" > { displayUsers } </ol>
 
 
-            <ReactPaginate
-                previousLabel={"Previous"}
-                nextLabel={"Next"}
-                pageCount={pageCount}
-                onPageChange={changePage}
-                containerClassName={"paginationBttns"}
-                previousLinkClassName={"previousBttn"}
-                nextLinkClassName={"nextBttn"}
-                disabledClassName={"paginationDisabled"}
-                activeClassName={"paginationActive"}
-            />
-        </div>
+        <ReactPaginate previousLabel = { "Previous" }
+        nextLabel = { "Next" }
+        pageCount = { pageCount }
+        onPageChange = { changePage }
+        containerClassName = { "paginationBttns" }
+        previousLinkClassName = { "previousBttn" }
+        nextLinkClassName = { "nextBttn" }
+        disabledClassName = { "paginationDisabled" }
+        activeClassName = { "paginationActive" }/>
+                </div >
     )
 }
 
--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -2,7 +2,14 @@ import React , {Component} from 'react';
 
 class Test extends Component {
     render() {
-        return super.render();
+        return super.render(
+            <div>
+                <span><sub>Author {post.author_id} .  posted in : {post.created_at} </sub></span>
+                <div> {post.description} </div>
+            </div>
+
+
+        );
     }
 }
 export default Test;
